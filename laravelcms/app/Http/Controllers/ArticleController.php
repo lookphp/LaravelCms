86f6627b17296e060c116e5dbffc9b2df9fb0f3e
qinjk <qinjk@alltosun.com>
2016-05-29 14:41:41 +0800

    add article model controller

--- laravelcms/app/Http/Controllers/ArticleController.php
+++ laravelcms/app/Http/Controllers/ArticleController.php
@@ -8,6 +8,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Models\ArticleContent;
+use App\Models\ArticleCategory;
 
 class ArticleController extends Controller{
     /**
@@ -17,7 +20,6 @@ class ArticleController extends Controller{
     {
         $article_id = ($req->id) ? $req->id : 0;
 
-        exit($article_id);
         //渲染模板
         return view('article.add', ['id'=>$article_id]);
     }
@@ -28,6 +30,62 @@ class ArticleController extends Controller{
      */
     public function postCreate(Request $req)
     {
-        exit('create');
+        $this->validate($req, [
+            'title' => 'required|max:255',
+            'article_content' => 'required',
+        ]);
+
+        //更改文章
+        $article  = new Article();
+        $content  = new ArticleContent();
+        $category = new ArticleCategory();
+        if ($req->id) {
+            $article_id   = $req->id;
+            $article_info = $article->find($req->id);
+
+            $article->save();
+        } else {
+            $article->title = $req->title;
+            $article->intro = $req->intro;
+            $article->save();
+            $article_id = $article->id;
+        }
+
+        //保存文章分类，先检查原先分类的状态，如果存在的
+
+        $category->article_id = $article->id;
+        $category->category_id = $req->category;
+        $category_res = $category->save();
+        if (!$category_res) {
+            return '保存分类失败';
+        }
+
+        //保存文章内容
+        $content = new ArticleContent();
+        $content->article_id = $article_id;
+        $content->content    = $req->article_content;
+        $res = $content->save();
+
+        if ($res) {
+            return '更新成功';
+        } else {
+            return '更新失败';
+        }
+    }
+
+    /**
+     * 更新文章
+     */
+    public function postUpdate()
+    {
+
+    }
+
+    /**
+     * 删除文章
+     */
+    public function getDel()
+    {
+
     }
 }
